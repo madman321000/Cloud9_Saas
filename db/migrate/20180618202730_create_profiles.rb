2e667810acc2816ca1e83917bbaacea550acc0f8
Maneesh Madala <maneesh.madala@gmail.com>
2018-06-18 20:32:49 +0000

    Fixed profile table

--- db/migrate/20180618202730_create_profiles.rb
+++ db/migrate/20180618202730_create_profiles.rb
@@ -1,5 +1,6 @@
 class CreateProfiles < ActiveRecord::Migration[5.0]
   def change
+    drop_table :profiles
     create_table :profiles do |t|
       t.integer :user_id
       t.string :first_name
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180618191529) do
+ActiveRecord::Schema.define(version: 20180618202730) do
 
   create_table "contacts", force: :cascade do |t|
     t.string   "name"
@@ -45,6 +45,15 @@ ActiveRecord::Schema.define(version: 20180618191529) do
   end
 
   create_table "profiles", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "job_title"
+    t.string   "phone_number"
+    t.string   "contact_email"
+    t.text     "description"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
   end
 
   create_table "users", force: :cascade do |t|
