ed4c8f0b065522afc3efba73c2fd5722eb8937c4
Maneesh Madala <maneesh.madala@gmail.com>
2018-06-18 19:15:59 +0000

    Added profiles table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180612220741) do
+ActiveRecord::Schema.define(version: 20180618191529) do
 
   create_table "contacts", force: :cascade do |t|
     t.string   "name"
@@ -44,6 +44,9 @@ ActiveRecord::Schema.define(version: 20180612220741) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "profiles", force: :cascade do |t|
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -58,7 +61,7 @@ ActiveRecord::Schema.define(version: 20180612220741) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.integer  "plan_id"
-    t.string   "stripe_customer_token"
+    t.string   "strike_customer_token"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
